df4a01733114f199d51fdae1c785d5a543bd1517
Christian Macht <christian.macht@gmail.com>
2024-01-26 05:52:28 +0100

    make it asgi

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 """
 Entry point for uwsgi.
 """
+from asgiref.wsgi import WsgiToAsgi
+
 from config import Config
 from src.start import create_app
 
 config = Config()
-app = create_app(config)
+app = WsgiToAsgi(create_app(config))
 
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=1024)
+# if __name__ == '__main__':
+#
+#     app.run(host='0.0.0.0', port=1024)
