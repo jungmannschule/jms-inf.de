2ed204e6461fd681fdeff52b1792eb9fafbd57eb
Christian Macht <christian.macht@gmail.com>
2025-01-08 07:34:11 +0100

    add criteria

--- src/routes/web7.py
+++ src/routes/web7.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 from flask import Blueprint, render_template
 from gitea import *
@@ -8,12 +9,34 @@ from src.models import Repository, Branch, Issue
 bp = Blueprint('web7', __name__)
 gitea = Gitea('https://jms-inf.de/git', os.getenv('GITEA_API'))
 
+@lru_cache()
+def get_repo_contents(repo):
+    print(repo.name)
+    commits = repo.get_commits()
+    return commits[0]
+
+
 @bp.route('/web7', methods=['GET'])
 def webseiten():
     org = Organization.request(gitea, '7inf')
     team = org.get_team('7c')
-    repos = team.get_repos()
-    return render_template('web7/overview.html', repos=[repo.name for repo in repos])
+    team_repos = team.get_repos()
+    repos = {}
+    # for repo in team_repos:
+        # c = get_repo_contents(repo)
+        # repos[repo.name] =
+    criteria = [
+        'Eine insgesamt stimmig umgesetzte Idee',
+        'Mindestens 3 HTML-Seiten, die korrekt benannt sind',
+        'Fehlerfreies HTML',
+        'Alle Seiten sind miteinander verlinkt',
+        'Mindestens eine Liste (ul/ol-Element) vorhanden',
+        '15 verschiedene CSS-Anweisungen',
+        # '5 verschiedene CSS-Selektoren',
+        'Mindestens zwei Bilder, per img-Element eingebunden',
+        'Den Workflow mit Git/Browser/Texteditor verstanden'
+    ]
+    return render_template('web7/overview.html', repos=[repo.name for repo in team_repos], criteria=criteria)
 
 
 def get_repo(user, repo):
